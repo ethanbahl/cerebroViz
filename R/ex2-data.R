89c07ddbe1bb88ef39abd8d0b8c8f5adcc581b6d
Tanner Koomar <tanner-koomar@uiowa.edu>
2016-06-28 12:22:09 -0500

    Moved and prepared external datasets (with devtools::usedata) so they can be accessed with lazyLoading.

---
+++ R/ex2-data.R
@@ -0,0 +1,12 @@
+#' Another example dataset for cerebroViz
+#'
+#' This matrix exmplifies the basic format cerebroViz requires of its input and includes overlapping brian regions.
+#'
+#' @docType data
+#'
+#' @usage ex2
+#'
+#' @format An object of class \code{"matrix"}
+#'
+#' @keywords datasets
+"ex2"
---
+++ R/regionMap.R
@@ -0,0 +1,12 @@
+#' Mapping between regions found in various brain databases & cerebroViz
+#'
+#' This dataframe is a reference for the naming conventions of cereBroviz as well as Brainspan, GteX and Epigenomics Roadmap.
+#'
+#' @docType data
+#'
+#' @usage regionMap
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @keywords datasets
+"regionMap"
