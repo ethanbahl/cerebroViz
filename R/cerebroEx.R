75c0383ebf54836cc81c33bf40614757122d202c
Tanner Koomar <tanner-koomar@uiowa.edu>
2016-08-10 14:40:57 -0500

    Minor changes to the origional dataset documentation.

--- R/cerebroEx.R
+++ R/cerebroEx.R
@@ -1,6 +1,6 @@
 #' An example dataset for cerebroViz
 #'
-#' This matrix exemplifies the basic format cerebroViz requires of its input. The data is raw expression levels for the POGZ gene taken from the Brainspan developmental transcriptome.
+#' This matrix exemplifies the basic format cerebroViz requires of its input. The data is raw log2 RPKM expression levels for the POGZ gene taken from the Brainspan developmental transcriptome.
 #'
 #' BrainSpan: Atlas of the Developing Human Brain [Internet]. Funded by ARRA Awards 1RC2MH089921-01, 1RC2MH090047-01, and 1RC2MH089929-01. © 2011. Available from: http://developinghumanbrain.org.
 #' @docType data
